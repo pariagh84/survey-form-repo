999940cfffae8200334d73a897ae1aeb8f3ca3e4
sanji <zero.0102003@gmail.com>
2025-02-27 21:49:03 +0330

    delete setBonus lines

--- survey-form/src/packages/informationPage.java
+++ survey-form/src/packages/informationPage.java
@@ -20,39 +20,29 @@ public class informationPage {
 
         nameLabel.setBounds(10, 10, 100, 20);
         JTextField nameField = new JTextField(15);
-        nameField.setBounds(50, 10, 100, 20);
 
         // Age Label and Text Field
         JLabel ageLabel = new JLabel("Age");
         ageLabel.setFont(font);
-        ageLabel.setBounds(10, 50, 100, 20);
         JTextField ageField = new JTextField(15);
-        ageField.setBounds(50, 50, 100, 20);
 
         // Email Label and Text Field
         JLabel emailLabel = new JLabel("Email");
         emailLabel.setFont(font);
-        emailLabel.setBounds(10, 90, 100, 20);
         JTextField emailField = new JTextField(15);
-        emailField.setBounds(50, 90, 100, 20);
 
         // Software Familiarity Level
         JLabel levelLabel = new JLabel("Level of familiarity with similar software");
         levelLabel.setFont(font);
-        levelLabel.setBounds(10, 130, 250, 20);
 
         JRadioButton level1 = new JRadioButton("Beginner");
         level1.setFont(font);
-        level1.setBounds(10, 160, 100, 20);
         JRadioButton level2 = new JRadioButton("Intermediate");
         level2.setFont(font);
-        level2.setBounds(100, 160, 100, 20);
         JRadioButton level3 = new JRadioButton("Advanced");
         level3.setFont(font);
-        level3.setBounds(190, 160, 100, 20);
         JRadioButton level4 = new JRadioButton("No familiarity");
         level4.setFont(font);
-        level4.setBounds(280, 160, 150, 20);
 
         // Group the radio buttons
         ButtonGroup group = new ButtonGroup();
@@ -64,7 +54,6 @@ public class informationPage {
         // Next Page Button
         JButton nextButton = new JButton("Next Page");
         nextButton.setFont(font);
-        nextButton.setBounds(10, 200, 100, 20);
         nextButton.addActionListener(e -> {
             // Remove current panel and move to Page2
             frame.remove(panel);
